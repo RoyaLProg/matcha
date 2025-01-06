7a05ba010bcb5c1db865788c75286776a0b94250
ThzEdouard <edouard.flaquet@gmail.com>
2025-01-06 16:57:52 +0100

    init database create tabel user tgypeorm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+	imports: [TypeOrmModule.forRoot({
+		type: 'postgres' as any,
+		host: 'localhost',
+		port: 5432,
+		username: process.env.POSTGRES_USER,
+		password: process.env.POSTGRES_PASSWORD,
+		database: process.env.POSTGRES_DB,
+		entities: [__dirname + '/**/*.entity{.ts,.js}'],
+		synchronize: true,
+	})],
   controllers: [AppController],
   providers: [AppService],
 })
