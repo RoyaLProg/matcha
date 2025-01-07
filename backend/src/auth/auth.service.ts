a3974decdf5c91806a092a6d73da6bca5f000e8b
ThzEdouard <edouard.flaquet@gmail.com>
2025-01-07 17:07:48 +0100

    backend login function

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import Users from 'src/entities/users.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 
 @Injectable()
 class AuthService {
@@ -9,10 +9,25 @@ class AuthService {
 	constructor(
 		@InjectRepository(Users)
 		private readonly UsersRepository: Repository<Users>,
+		private dataSources: DataSource
 	) {}
 
 	async addUser(user: Users): Promise<Users> {
 		return await this.UsersRepository.save(user);
+	}
+
+	async getLogin(username: string, password: string): Promise<Omit<Users, 'password'> | undefined> {
+		const user = await this.dataSources.getRepository(Users).findOne({ where: { username: username }});
+		if (!user)
+			throw new Error('user not found');
+		if (user.password !== password)
+			throw new Error('password not correct');
+		if (!user.isValidated)
+			throw new Error('user not validated');
+		if (user.biography === "")
+			return undefined;
+		const { password: _, ...userWithoutPassword } = user;
+		return userWithoutPassword;
 	}
 }
 
