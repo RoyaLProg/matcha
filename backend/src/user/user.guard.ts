e7ac953b4c6dc135643162f026f46643468c8fd9
Camille CAMBIUM <ccambium@paul-f4Br5s4.clusters.42paris.fr>
2025-01-27 16:44:13 +0100

    feat(backend): userGuard

---
+++ backend/src/user/user.guard.ts
@@ -0,0 +1,37 @@
+import { Injectable, CanActivate, ExecutionContext } from "@nestjs/common";
+import { Request } from "express";
+import { Observable } from "rxjs";
+import { JwtService } from "@nestjs/jwt";
+
+@Injectable()
+export default class UserGuard implements CanActivate {
+	constructor(
+		private readonly jwtService: JwtService,
+	){}
+
+	validateRequest(request: Request): boolean {
+		const jwt = request.cookies['auth'];
+		
+		try {
+			const jwtDecoded = this.jwtService.verify(jwt, {secret: process.env.JWT_SECRET});
+			const path = request.originalUrl.split('/');
+			const id = Number(path[path.length - 1]);
+
+			console.log(id, jwtDecoded);
+
+			if (!id)
+				return true;
+
+			if (id !== jwtDecoded['id'])
+				return false;
+		} catch (e) {
+			return false;
+		}
+		return true
+	}
+	
+	canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+		const request = context.switchToHttp().getRequest();
+		return this.validateRequest(request);
+	}
+}
