a46735c786a8854ccf434e54e68b0a25a358867c
camille CAMBIUM <royalfr@protonmail.com>
2025-01-30 09:41:18 +0100

    feat(frontend): match page

--- frontend/src/routes/home/Home.tsx
+++ frontend/src/routes/home/Home.tsx
@@ -12,26 +12,26 @@ export function Home() {
 				</div>
 				<div className="myProfile">
 					<img src="https://www.w3schools.com/w3images/avatar2.png" alt="profile" />
+					<Link to={"/chatlist"}><button className="chatButton"> <span className="material-symbols-outlined">chat</span> </button></Link>
 				</div>
 			</div>
-			<div className="content">
-				<div className="yourListPicture">
-					<span className="material-symbols-outlined">arrow_back_ios</span>
-					<img src="https://www.w3schools.com/w3images/avatar2.png" alt="profile" />
-					<span className="material-symbols-outlined">arrow_forward_ios</span>
+			<div id="container">
+				<button className="dislikeButton"> <span className="material-symbols-outlined">thumb_down</span> </button>
+				<div className="content">
+					<div className="card">
+						<div className="yourListPicture">
+							<span className="material-symbols-outlined">arrow_back_ios</span>
+							<img src="https://www.w3schools.com/w3images/avatar2.png" alt="profile" />
+							<span className="material-symbols-outlined">arrow_forward_ios</span>
+						</div>
+						<div className="yourProfile">
+							<p style={{fontSize: "32px"}}>Name, Age ans</p>
+							<p style={{fontSize: "20px", color: "#aaa"}}>Pays, Ville (distance)</p>
+							<p style={{fontSize: "15px", textAlign: "justify"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac consequat magna. Nunc lorem ligula, placerat sed molestie sed, faucibus ut magna. Aenean vitae auctor quam. Vivamus in tempor eros. Nunc metus felis, malesuada vitae lorem eu, finibus rutrum nibh. Proin ipsum lectus, accumsan eu lectus auctor, vulputate imperdiet nunc. Morbi viverra, quam at pulvinar eleifend, magna arcu condimentum justo, in condimentum felis dolor vitae diam. Morbi sed gravida sapien, sit amet porttitor nulla. Mauris et ullamcorper lorem. Sed ligula arcu, volutpat ac ex vel, semper tempus lacus. </p>
+						</div>
+					</div>
 				</div>
-				<div className="yourProfile">
-					<p> Localisation : </p>
-					<p> Name : </p>
-					<p> Age : </p>
-					<p> Bio : </p>
-					<p> Tags : </p>
-				</div>
-				<div className="Button">
-					<button className="dislikeButton"> <span className="material-symbols-outlined">thumb_down</span> </button>
-					<Link to={"/chatlist"}><button className="chatButton"> <span className="material-symbols-outlined">chat</span> </button></Link>
-					<button className="likeButton"> <span className="material-symbols-outlined">thumb_up</span> </button>
-				</div>
+				<button className="likeButton"> <span className="material-symbols-outlined">thumb_up</span> </button>
 			</div>
 		</div>
 	);
