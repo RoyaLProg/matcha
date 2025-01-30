ea7cee15038a9aa7e6645c1893d749f6e475b1a3
ThzEdouard <edouard.flaquet@gmail.com>
2025-01-30 13:08:31 +0100

    fix chat context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,17 +5,14 @@ import App from './App.tsx'
 import UserProvider from './context/UserContext.tsx'
 import WebSocketProvider from './context/WebSocketContext.tsx'
 import ChatsProvider from './context/ChatsContext.tsx'
-import ChatProvider from './context/ChatContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<UserProvider>
 			<WebSocketProvider>
-				{/* <ChatsProvider> */}
-					{/* <ChatProvider> */}
-						<App />
-					{/* </ChatProvider> */}
-				{/* </ChatsProvider> */}
+				<ChatsProvider>
+					<App />
+				</ChatsProvider>
 			</WebSocketProvider>
 		</UserProvider>
 	</StrictMode>,
